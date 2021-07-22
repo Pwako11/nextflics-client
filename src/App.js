1ba43b41ef1220619095222ddf0134d1475090de
Pwako11 <akolo@hotmail.com>
2021-07-22 01:01:03 -0400

    removed componentDidMount

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
-  componentDidMount() {
-    fetch("http://localhost:3000/api/v1/users/")
-    .then( r => r.json())
-    .then(console.log)
-
-  }
+  
   render(){
     return (
       <div className="App">
