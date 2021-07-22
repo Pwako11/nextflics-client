b871efba21e7215af4122003343a4f91bea6a580
Pwako11 <akolo@hotmail.com>
2021-07-22 12:26:14 -0400

    Store file for redux state

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import usersReducers from './reducers/users.js';
+
+const reducer = combineReducers({
+    user: usersReducers
+  })
+  
+  const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+  const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
