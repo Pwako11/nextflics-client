fd12f803fbb6829c5328626494a6d09aca028fd9
Pwako11 <akolo@hotmail.com>
2021-07-24 07:33:26 -0400

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 import './App.css';
 import Login from "./components/Login.js"
+import Logout from "./components/Logout.js"
 import {connect} from "react-redux"
 import {getCurrentUser} from "./actions/currentUser.js"
 
 class App extends React.Component{
   
-  componentDidMount(){
+  componentDidMount() {
     this.props.getCurrentUser()
   }
 
 
   render(){
     return (
-      <Login />
+      this.props.currentUser ? <Logout /> : <Login />
     );
   }
 }
 
-export default connect(null, {getCurrentUser}) (App);
+const mapStateToProps = ({currentUser}) => {
+  return{
+    currentUser
+  }
+}
+export default connect(mapStateToProps, {getCurrentUser}) (App);
