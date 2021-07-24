c9a7232ccdc3717393db89db5ec9e2bfc2824c31
Pwako11 <akolo@hotmail.com>
2021-07-24 04:12:05 -0400

    added current user action

---
+++ src/actions/currentUser.js
@@ -0,0 +1,53 @@
+export const setCurrentUser = user =>{
+    return {
+        type: "SET_CURRENT_USER",
+        user
+    }
+}
+
+
+export const login = credentials => {
+    console.log("Credentials are", credentials)
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/login", {
+            credentials: "include",
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then( response => response.json())
+        .then(user =>{
+            if(user.error){
+                alert(user.error)
+            }else{
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const getCurrentUser = () =>{
+    console.log("Dispatching get current user")
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/get_current_user", {
+            credentials: "include",
+            method: "GET",
+            headers:{
+                "Content-Type": "application/json"
+            },
+        })
+        .then( response => response.json())
+        .then(user =>{
+            if(user.error){
+                alert(user.error)
+            }else{
+                dispatch(setCurrentUser(user))
+                console.log("in get current user fetch", user)
+            }
+        })
+        .catch(console.log)
+    }
+}
