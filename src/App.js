353eeaf2995486c7083f9e6d6662a3fcdcae675f
Pwako11 <akolo@hotmail.com>
2021-07-24 03:59:23 -0400

    Added connect react-redux, login and componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 import './App.css';
+import Login from "./components/Login.js"
+import {connect} from "react-redux"
+import {getCurrentUser} from "./actions/currentUser.js"
 
 class App extends React.Component{
   
+  componentDidMount(){
+    this.props.getCurrentUser()
+  }
+
+
   render(){
     return (
-      "Your app is running"
+      <Login />
     );
   }
 }
 
-export default App;
+export default connect(null, {getCurrentUser}) (App);
