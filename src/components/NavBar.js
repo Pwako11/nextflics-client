424d4ff63106f734e7e24a7bcd048333d1a8c5c0
Pwako11 <akolo@hotmail.com>
2021-07-25 04:51:11 -0400

    Added a NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+// import { render } from '@testing-library/react'
+import React from 'react' 
+import {connect} from 'react-redux'
+import Login from "./Login.js"
+import Logout from "./Logout.js"
+
+const NavBar = ({ currentUser }) => {
+    
+    return (
+        <div className= "nav">
+          
+            { currentUser ? `Welcome ${currentUser.name} ` : "" }   
+            { currentUser ? <Logout /> : <Login /> } 
+
+        </div>
+    )
+}
+
+const mapStateToProps = ({currentUser}) => {
+    return{
+      currentUser
+    }
+  }
+
+export default connect(mapStateToProps) (NavBar)
