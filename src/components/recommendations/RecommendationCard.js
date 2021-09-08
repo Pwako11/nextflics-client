a0d8e3668156abd0e3c96357c8b24b085cde4251
Pwako11 <akolo@hotmail.com>
2021-09-08 01:07:07 -0400

    removed console.log's

--- src/components/recommendations/RecommendationCard.js
+++ src/components/recommendations/RecommendationCard.js
@@ -3,20 +3,17 @@ import {connect} from 'react-redux';
 import {deleteRecommendation} from '../../actions/recommendations';
 
 
-const RecommendationCard = ({recommendations, movies, recommendation, history, deleteRecommendation}) =>{
-
-    
-
-    const movieID = recommendation.relationships.movie.data.id
-    const recommendedMovie = movies.find(element => element.id == movieID);
-    const moviePoster = recommendedMovie.attributes.poster_path
-    const movieName = recommendation.attributes.name 
-        
+const RecommendationCard = (props) =>{
    
+    const movieID = props.recommendation.relationships.movie.data.id
+    const recommendedMovie = props.movies.find(element => element.id == movieID);
+    const moviePoster = recommendedMovie.attributes.poster_path
+    const movieName = props.recommendation.attributes.name 
+        
     return(
         <div className="recommendationCard">
 
-            <p>{movieName} <br/> <img src={moviePoster} alt={movieName} className="movie-poster"  width="250" height="325" /><br/><button  class="btn btn-secondary" onClick={()=>deleteRecommendation(recommendations, recommendation, history)}>Delete this Recommendation</button>  <button className="btn btn-secondary" onClick={() => history.goBack()}>Back</button></p>
+            <p>{movieName} <br/> <img src={moviePoster} alt={movieName} className="movie-poster"  width="250" height="325" /><br/><button  class="btn btn-secondary" onClick={()=>props.deleteRecommendation(props.recommendations, props.recommendation, props.history)}>Delete this Recommendation</button>  <button className="btn btn-secondary" onClick={() => props.history.goBack()}>Back</button></p>
            
         </div>
     ) 
