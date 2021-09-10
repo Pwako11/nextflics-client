1e6e1f25d0d2877913945b02811c277d7a3ab952
Pwako11 <akolo@hotmail.com>
2021-09-10 00:58:01 -0400

    Added a sorting feature

--- src/components/movies/MovieShowcase.js
+++ src/components/movies/MovieShowcase.js
@@ -3,17 +3,37 @@ import {connect} from 'react-redux';
 import MovieCard from './MovieCard.js';
 
 const MovieShowcase = ({cards, loggedIn}) => { 
-
+    console.log({cards})
     const [showMovies, setShowMovies] = useState(false);
-    
+    const [alphaCards, alphaSortList] = useState([]);
+   
+    console.log({alphaCards})
+   
     const handleChange = () =>{
         setShowMovies(!showMovies)
     }
 
-    const movieCard = cards.length > 0 ? cards.map(  function(list) {
+    const aSortMovies = () => {
+        console.log("you are in aSorted")
+       
+
+        const sortedCards = cards.sort((a,b) =>{
+            console.log({alphaCards})
+            if(a.attributes.title < b.attributes.title) return -1;
+            if(a.attributes.title > b.attributes.title) return 1;
+            return 0;
+        });
+        console.log({sortedCards})
+        alphaSortList(sortedCards)
+        console.log({alphaCards})
+    }
+
+    const movieCard = alphaCards.length > 0 ? alphaCards.map( function(list) {
         return <MovieCard key={list.id} card={list} />
         })
-        : null;
+        : cards.map( function(list) {
+            return <MovieCard key={list.id} card={list} />
+            });
 
     const optoinOne = <>
         <button className="btn btn-secondary" id= "movieButton" onClick={handleChange}>{showMovies? 'hide movies' : 'show Movies'}</button>
@@ -21,9 +41,11 @@ const MovieShowcase = ({cards, loggedIn}) => {
 
     const movieButton = loggedIn ? optoinOne : <div id= "movie_showcase">{ movieCard }</div>  
 
+    const sortMoviesA = <button className="btn btn-secondary" id= "sortButton" onClick={aSortMovies}>Sort Movies A - Z </button>
+ 
     return (
        <div className= "movie-showcase" >
-             {movieButton}
+            {movieButton}{sortMoviesA}
         </div>
     )
 }
